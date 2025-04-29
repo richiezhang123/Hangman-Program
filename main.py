21a3f2c41a5757eb2b72fd2da2e13b46cdc21d12
richiezhang123 <22035@students.mrgs.school.nz>
2025-04-29 12:02:34 +1200

    Created a menu page window which included labels, an entry box for the user to input their name, a continue button and a function to collect the user's name

--- main.py
+++ main.py
@@ -2,3 +2,33 @@ from tkinter import *
 
 names = []
 words = []
+
+class MenuPage:
+    def __init__(self, parent):
+        self.quiz_frame= Frame(parent, padx = 100, pady = 100)
+        self.quiz_frame.grid()
+
+        self.heading_label = Label(self.quiz_frame, text="HANGMAN", font=("Tw Cen Mt", "36", "bold"))
+        self.heading_label.grid()
+
+        self.user_label = Label(self.quiz_frame,text="Enter your username:", font=("Tw Cen Mt", "16"))
+        self.user_label.grid()
+
+        self.entry_box = Entry(self.quiz_frame)
+        self.entry_box.grid()
+
+        self.continue_button = Button(self.quiz_frame, text = "Continue", command = self.name_collection)
+        self.continue_button.grid()
+
+    def name_collection(self):
+        global name
+        name = self.entry_box.get()
+        names.append(name)
+        self.quiz_frame.destroy()
+        Hangman(root)
+
+if __name__ == "__main__":
+    root = Tk()
+    root.title("Hangman")
+    hangman_instance = MenuPage(root)
+    root.mainloop()
